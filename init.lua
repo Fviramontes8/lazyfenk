10cb19584fb26f33e65770e857b37cc36cbe3993
Francisco Viramontes <francisco.viramontes@cosmiac.org>
2024-10-10 13:07:14 -0600

    Add custom options and keybinds

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -157,6 +157,20 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Fenk's additional options
+
+-- Show matching (), [], {}
+vim.opt.showmatch = true
+
+-- Ignore autocomplete for .o files
+vim.opt.wildignore = '*.o'
+
+-- Incremental search
+vim.opt.incsearch = true
+
+-- Draws a column at position 80
+vim.opt.colorcolumn = '80'
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -189,6 +203,10 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Fenk's keybinds
+vim.keymap.set('n', '<leader>j', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>r', ':wincmd r<CR>')
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
